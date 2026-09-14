17433aa4d87c7e987b6d7d4fbe854f6d464e10b0
gamzatanghulu <kimjunyoo9@gmail.com>
2026-09-15 08:41:29 +0900

    fix: 수정내용 적기

--- config/sensorConfig.js
+++ config/sensorConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
   HASH_KEY: process.env.HASH_KEY || '75597298595310529218112835277866',
-  RECEIVER_TO: process.env.RECEIVER_TO || "yhlohna@choheung.co.kr",
-  RECEIVER_CC: process.env.RECEIVER_CC || "joker@choheung.co.kr; ohdonga@choheung.co.kr; moonflair@choheung.co.kr; hojin3131@choheung.co.kr; sh_yoon001@choheung.co.kr; su25@choheung.co.kr; yoonsook.ko@choheung.co.kr; iopen009@choheung.co.kr",
+  // RECEIVER_TO: process.env.RECEIVER_TO || "yhlohna@choheung.co.kr",
+  // RECEIVER_CC: process.env.RECEIVER_CC || "joker@choheung.co.kr; ohdonga@choheung.co.kr; moonflair@choheung.co.kr; hojin3131@choheung.co.kr; sh_yoon001@choheung.co.kr; su25@choheung.co.kr; yoonsook.ko@choheung.co.kr; iopen009@choheung.co.kr",
   FETCH_INTERVAL_MS: 5 * 60 * 1000, // 5분
+
 
   JOA_CONFIG: {
     id: process.env.JOA_ID || '01095052917',
@@ -16,7 +17,11 @@ module.exports = {
     '2team1':       { name: '쿠커실', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '7244-3574' },
     '2team':        { name: '유화솥', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '4289-4748' },
     '2팀천장':        { name: '2팀천장', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '5704-7896' },
-    '스마트센서':   { name: '13번창고', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '8433-5905', channel: 1 },
+    '스마트센서':   { name: '12번창고', zone: '외부창고', type: 'FREEZING', min: -25.0, max: -12.0, sensorId: '8433-5905', channel: 1},
+    '2채널':   { name: '13번창고', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '8433-5905', channel: 2 },
+    'B동 냉동':   { name: 'B동냉동창고', zone: '외부창고', type: 'FREEZING', min: -25.0, max: -12.0, sensorId: '8405-9325'},
+    'B동 냉장1,2':   { name: 'B동냉장창고1', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '2830-9035', channel: 1},
+    'B동 냉장1,2':   { name: 'B동냉장창고2', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '2830-9035', channel: 2},
     'joa_co2':      { name: '탄산 고압용기', zone: '가스 저장소', type: 'GAS' },
     'joa_n2':       { name: '질소 고압용기', zone: '가스 저장소', type: 'GAS' }
   }
--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ const SENSOR_CONFIG = {
   '2team1':       { name: '쿠커실', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '7244-3574' },
   '2team':        { name: '유화솥', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '4289-4748' },
   '2팀천장':      { name: '2팀천장', zone: '현장 온열', type: 'OUTDOOR', min: 0.0, max: 36.0, sensorId: '5704-7896' },
-  '스마트센서':   { name: '13번창고', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '8433-5905' },
-  '냉동센서1':    { name: '냉동 1라인', zone: 'B1 냉동동', type: 'FREEZING', min: -25.0, max: -18.0, sensorId: '' }
+  '스마트센서':   { name: '12번창고', zone: '외부창고', type: 'FREEZING', min: -25.0, max: -12.0, sensorId: '8433-5905', channel: 1 },
+  '2채널':   { name: '13번창고', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '8433-5905', channel: 2 },
+  'B동 냉동':   { name: 'B동냉동창고', zone: '외부창고', type: 'FREEZING', min: -25.0, max: -12.0, sensorId: '8405-9325'},
+  'B동 냉장1,2':   { name: 'B동냉장창고1', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '2830-9035', channel: 1},
+  'B동 냉장1,2':   { name: 'B동냉장창고2', zone: '외부창고', type: 'COOLING', min: 0.0, max: 5.0, sensorId: '2830-9035', channel: 2},
 };
 
 // 전역 설정
