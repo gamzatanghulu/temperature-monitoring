f245370adf64d4ed6ab8ea4af9a94cdd74b1004e
gamzatanghulu <kimjunyoo9@gmail.com>
2026-09-15 08:42:22 +0900

    fix: 수정내용 적기

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const SENSOR_CONFIG = {
 
 // 전역 설정
 const CONFIG = {
-  API_BASE_URL: 'https://pays-tomato-robert-shield.trycloudflare.com',
+  API_BASE_URL: 'https://combining-black-mainly-north.trycloudflare.com',
   ALARM_DURATION_SEC: 5,
   POLLING_INTERVAL_MS: 5000,
   EXCLUDED_ALERT_TYPES: ['GAS']
